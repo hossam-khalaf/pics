071df3cf31e7456c115006371ce58caab7b3b05a
hossam-khalaf <hossamhassan303089@gmail.com>
2021-01-09 09:50:28 +0200

    semantic ui cdn && basic styling

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import SearchBar from './SearchBar'
 
 const App = () => {
 	return (
-		<div>
+		<div className='ui container' style={{ marginTop: '40px' }}>
 			<SearchBar />
 		</div>
 	)
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,9 +3,17 @@ import React, { Component } from 'react'
 class SearchBar extends Component {
 	render() {
 		return (
-			<div>
-				<form>
-					<input type='text' name='' id='' placeholder='hey search here' />
+			<div className='ui segment'>
+				<form className='ui form'>
+					<div className='field'>
+						<label htmlFor='imageSearch'>Image Search</label>
+						<input
+							type='text'
+							name='imageSearch'
+							id='imageSearch'
+							placeholder='hey search here'
+						/>
+					</div>
 				</form>
 			</div>
 		)
